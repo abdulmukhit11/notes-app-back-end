468cc1c13f91e23902d739b53da3f189a843fddc
Abdul <abdulmukhit11@gmail.com>
2022-12-18 19:47:49 +0700

    fix bugs host value

--- src/server.js
+++ src/server.js
@@ -17,7 +17,7 @@ const init = async () => {
   server.route(routes);
 
   await server.start();
-  console.log(`Server berjalan pada ${server.info.uri}`);
+  console.log(`Server berjalan pada ${server.info.uri}`);
 };
 
 init();
