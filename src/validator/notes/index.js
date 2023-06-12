86904ccc51c64fd1e4b8e9250af90191e6d097f9
muchittt <abdulmukhit11@gmail.com>
2023-06-12 07:35:28 +0700

    restructure with hapi plugin

--- src/validator/notes/index.js
+++ src/validator/notes/index.js
@@ -3,5 +3,10 @@ const { NotePayloadSchema } = require('./schema');
 const NotesValidator = {
     validateNotePayload: (payload) => {
         const validationResult = NotePayloadSchema.validate(payload);
+        if (validationResult.error) {
+            throw new Error(validationResult.error.message);
+        }
     },
 };
+
+module.exports = NotesValidator;
