16c63cf5609663e3752b751aa06d43409ae84a48
muchittt <abdulmukhit11@gmail.com>
2023-06-29 05:14:30 +0700

    commit

--- src/services/postgres/NotesService.js
+++ src/services/postgres/NotesService.js
@@ -1,6 +1,7 @@
 const { Pool } = require('pg');
 const { nanoid } = require('nanoid');
 const InvariantError = require('../../exceptions/InvariantError');
+const { mapDBToModel } = require('../../utils');
      
 class NotesService {
   constructor() {
@@ -13,10 +14,48 @@ class NotesService {
     const updatedAt = createdAt;
 
     const query = {
-        text: 'INSERT INTO notes VALUES($1, $2, $3, $4, $5, $6) RETURNING id',
-        values: [id, title, body, tags, createdAt, updatedAt],
-      };
+      text: 'INSERT INTO notes VALUES($1, $2, $3, $4, $5, $6) RETURNING id',
+      values: [id, title, body, tags, createdAt, updatedAt],
+    };
 
-      const result = await this._pool.query(query);
+    const result = await this._pool.query(query);
+  }
+  
+  async getNotes() {
+    const result = await this._pool.query('SELECT * FROM notes');
+    return result.rows.map(mapDBToModel);
+  }
+
+  async getNoteById(id) {
+    const query = {
+      text: 'SELECT * FROM notes WHERE id = $1',
+      values: [id],
+    };
+    const result = await this._pool.query(query);
+ 
+    if (!result.rows.length) {
+      throw new NotFoundError('Catatan tidak ditemukan');
     }
+ 
+    return result.rows.map(mapDBToModel)[0];
+
+    if (!result.rows.length) {
+      throw new NotFoundError('Gagal memperbarui catatan. Id tidak ditemukan');
+    }
+  }
+
+  async deleteNoteById(id) {
+    const query = {
+      text: 'DELETE FROM notes WHERE id = $1 RETURNING id',
+      values: [id],
+    };
+ 
+    const result = await this._pool.query(query);
+ 
+    if (!result.rows.length) {
+      throw new NotFoundError('Catatan gagal dihapus. Id tidak ditemukan');
+    }
+  }
 }
+
+module.exports = NotesService;
